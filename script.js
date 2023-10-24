320e30994dcffe00ab39759d904351c99c40b2d6
Yosua <yosua.halim@gmail.com>
2023-10-24 22:48:28 +0700

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function handleClick() {
   window.alert("Hello, world!");
 }
+function handleSubmit(e, test) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+
+  window.alert(`Hello, ${test}!`);
+}
